9065f8416fa2b82f31b41e30505dc7d8573ef908
acferten <gowiththeirname@gmail.com>
2022-11-22 12:22:23 +0700

    продукты, регистрация по api

--- www/app/Controller/Api.php
+++ www/app/Controller/Api.php
@@ -2,21 +2,44 @@
 
 namespace Controller;
 
-use Model\Post;
+use Model\Product;
+use Model\User;
 use Src\Request;
+use Src\Validator\Validator;
 use Src\View;
 
 class Api
 {
     public function index(): void
     {
-        $posts = Post::all()->toArray();
-
+        $posts = Product::all()->toArray();
         (new View())->toJSON($posts);
     }
 
     public function echo(Request $request): void
     {
         (new View())->toJSON($request->all());
+    }
+
+    public function signup(Request $request)
+    {
+        $validator = new Validator($request->all(), [
+            'name' => ['required'],
+            'login' => ['required', 'unique:users,login'],
+            'password' => ['required']
+        ], [
+            'required' => 'Поле :field пусто',
+            'unique' => 'Поле :field должно быть уникально'
+        ]);
+
+        if ($validator->fails()) {
+            (new View())->toJSON(['message' => json_encode($validator->errors(), JSON_UNESCAPED_UNICODE)]);
+        }
+
+        if (User::create($request->all())) {
+            (new View())->toJSON(['message' => "Пользователь создан"]);
+        }
+
+
     }
 }
